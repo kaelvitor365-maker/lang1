73034a4c0899085898b44e3778b1d62031a269f1
Phanta <kaelvitor365@gmai.com>
2026-07-05 19:15:37 -0300

    chore: configurando o branch

--- utils/defines/def.h
+++ utils/defines/def.h
@@ -5,3 +5,5 @@
 #define CAST_TO(type, val) ((type)(val))
 #define len(object) ((object)->__size)
 #define VECTOR_FOR_EACH(vector, type, var, body) for(size_t _i = 0; _i < len(vector); ++_i) { type* var = getVector(vector, _i); body}
+#define PTR(type) type* 
+#define USE_PTR(ptr) (*ptr) 
