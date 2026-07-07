20c61576d3f977a0e2247af5f59e35cde0b649fc
Phanta <kaelvitor365@gmai.com>
2026-07-06 23:08:59 -0300

    nothing

--- utils/defines/def.h
+++ utils/defines/def.h
@@ -14,3 +14,5 @@
 #define FUNCTIONS(name, ...) (*name)(__VA_ARGS__);
 #define CLASS(name, scope) INTERN_NAME(name) scope; typedef INTERN_NAME(name) name;
 #define ENUM(name, ...) typedef enum { __VA_ARGS__} name;
+
+#define WITH_LEXER(name, __arq, body) do{ Lexer* name = newLexer(__arq); body freeLexer(name); }while(0)
